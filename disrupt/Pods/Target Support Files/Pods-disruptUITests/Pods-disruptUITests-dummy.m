5a171f43cafaa6bfa776c2169a5c41481f49b68f
Kai-Li Yen <kailiyen@berkeley.edu>
2017-06-07 15:39:37 -0400

    set up google places api

---
+++ disrupt/Pods/Target Support Files/Pods-disruptUITests/Pods-disruptUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_disruptUITests : NSObject
+@end
+@implementation PodsDummy_Pods_disruptUITests
+@end
--- disrupt/disrupt/AppDelegate.swift
+++ disrupt/disrupt/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyBT6ONXk4Di8H0d01Zx5G3qW0A1IqJJuOc")
+        
+        GMSServices.provideAPIKey("AIzaSyBT6ONXk4Di8H0d01Zx5G3qW0A1IqJJuOc")
         return true
     }
 
