7037cfbe168a688c072c650f064692e98f827593
MichelleLu1995 <Michelle.lu@columbia.edu>
2017-06-08 01:16:44 -0400

    Added radius and category user select

--- disrupt/disrupt/ProfileViewController.swift
+++ disrupt/disrupt/ProfileViewController.swift
@@ -8,15 +8,72 @@
 
 import UIKit
 
-class ProjectViewController: UIViewController {
+
+
+
+class ProjectViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var TitleText: UILabel!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        var currentValue = Int(sender.value)
+        
+        label.text = "\(currentValue)"
+    }
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var pickerDataSource = ["Food", "Clothing", "Transportation", "Hotel", "Miscellaneous"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Project View"
+        
+        self.pickerView.dataSource = self;
+        self.pickerView.delegate = self;
+        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
     
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerDataSource[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        TitleText.text = pickerDataSource[row]
+    }
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
+//    
+//    
+//    
+//    @available(iOS 2.0, *)
+//    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//
+//    
+//    
+//    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+//        return 1
+//    }
+//    
+//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//        return pickerDataSource.count;
+//    }
+//    
+//    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+//        return pickerDataSource[row]
+//    }
+    
+
 }
