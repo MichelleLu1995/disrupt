315121800026b073b1cc34fccd214017d192988e
MichelleLu1995 <Michelle.lu@columbia.edu>
2017-06-08 05:12:34 -0400

    updated ui

---
+++ disrupt/disrupt/ListViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ListViewController.swift
+//  disrupt
+//
+//  Created by Lu, Michelle on 6/8/17.
+//  Copyright © 2017 Annette Chen. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController{
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+            setupSegmentedControl()
+            
+            updateView()
+    }
+
+    private func setupSegmentedControl() {
+        // Configure Segmented Control
+        segmentedControl.removeAllSegments()
+        segmentedControl.insertSegment(withTitle: "Map", at: 0, animated: true)
+        segmentedControl.insertSegment(withTitle: "List", at: 1, animated: true)
+        segmentedControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
+        
+        // Select First Segment
+        segmentedControl.selectedSegmentIndex = 1
+    }
+    func selectionDidChange(_ sender: UISegmentedControl) {
+        updateView()
+    }
+    
+    private lazy var MapViewController: MapViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    private lazy var TableViewController: TableViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "TableViewController") as! TableViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+
+    private func add(asChildViewController viewController: UIViewController) {
+        // Add Child View Controller
+        addChildViewController(viewController)
+        
+        // Add Child View as Subview
+        view.addSubview(viewController.view)
+        
+        // Configure Child View
+        viewController.view.frame = view.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Notify Child View Controller
+        viewController.didMove(toParentViewController: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        // Notify Child View Controller
+        viewController.willMove(toParentViewController: nil)
+        
+        // Remove Child View From Superview
+        viewController.view.removeFromSuperview()
+        
+        // Notify Child View Controller
+        viewController.removeFromParentViewController()
+    }
+
+    private func updateView() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            remove(asChildViewController: TableViewController)
+            add(asChildViewController: MapViewController)
+        } else {
+            remove(asChildViewController: TableViewController)
+            add(asChildViewController: MapViewController)
+        }
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+}
